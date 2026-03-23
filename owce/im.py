22bb5ecb2871fc77e1b5ef940b64acd008510347
Michal <45431934+MohuXylene@users.noreply.github.com>
2026-03-23 17:40:15 +0100

    delete unnecessary files and stop tracking doc files for now in .gitignore

--- owce/im.py
+++
@@ -1,6 +0,0 @@
-with open("names.txt", mode="r", encoding="UTF-8") as fileIn, open("names.py", mode="w", encoding="UTF-8") as fileOut:
-      fileOut.write("names = [")
-      for line in fileIn:
-            name = line.split("\n")[0]
-            fileOut.write("\"" + name + "\",")
-      fileOut.write("]")
